98d7cd15f61ff0c1bf64c59155bf5171b4697757
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-05-27 17:25:43 +0800

    better config

--- smartromsync/sy_config.py
+++ smartromsync/sy_config.py
@@ -1,5 +1,6 @@
 """Config loading, setup, validating, writing."""
 
+import datetime
 import json
 from pathlib import Path
 from typing import Self
@@ -8,6 +9,7 @@ import tomlkit
 from pydantic import BaseModel, model_validator
 from pydantic_settings import BaseSettings
 
+from . import PROGRAM_NAME, URL, __version__
 from .logger import get_logger
 
 # Logging should be all done at INFO level or higher as the log level hasn't been set yet
@@ -60,15 +62,31 @@ class ConfigDef(BaseSettings):
 
     def write_config(self, config_location: Path) -> None:
         """Write the current settings to a TOML file."""
-        logger.info("Writing config to %s", config_location)
-        config_data = json.loads(self.model_dump_json())  # This is how we make the object safe for tomlkit
+        config_location.parent.mkdir(parents=True, exist_ok=True)
 
-        # Write to the TOML file
-        if not config_location.parent.exists():
-            config_location.parent.mkdir(parents=True, exist_ok=True)
+        config_data = json.loads(self.model_dump_json())  # This is how we make the object safe for tomlkit
+        if not config_location.exists():
+            logger.warning("Config file does not exist, creating it at %s", config_location)
+            config_location.touch()
+            existing_data = config_data
+        else:
+            with config_location.open("r") as f:
+                existing_data = tomlkit.load(f)
+
+        logger.info("Writing config to %s", config_location)
+
+        new_file_content_str = f"# Configuration file for {PROGRAM_NAME} v{__version__} {URL}\n"
+        new_file_content_str += tomlkit.dumps(config_data)
+
+        if existing_data != config_data:  # The new object will be valid, so we back up the old one
+            time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
+            backup_file = config_location.parent / f"{config_location.stem}_{time_str}{config_location.suffix}.bak"
+            logger.warning("Validation has changed the config file, backing up the old one to %s", backup_file)
+            with backup_file.open("w") as f:
+                f.write(tomlkit.dumps(existing_data))
 
         with config_location.open("w") as f:
-            tomlkit.dump(config_data, f)
+            f.write(new_file_content_str)
 
 
 def load_config(config_path: Path) -> ConfigDef:
